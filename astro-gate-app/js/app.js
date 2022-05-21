0044af53714fc2d7ea6a74b74b9d3d0eb6a27a7c
kaitya <102527906+kaitya@users.noreply.github.com>
2022-05-21 22:07:30 +0300

    new files added

--- astro-gate-app/js/app.js
+++ astro-gate-app/js/app.js
@@ -1,27 +1,3 @@
-/* Слайдеры */
-
-let slideIndex = [1,1];
-
-let slideId = ["popup_slider", "page_slider"]
-
-showSlides(1, 0);
-showSlides(1, 1);
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
-
-function showSlides(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].classList.remove("shown");  
-  }
-  x[slideIndex[no]-1].classList.add("shown");  
-}
-
 $(document).ready(function () {
 
     /* Переключение пола (муж/жен) */
@@ -34,12 +10,14 @@ $(document).ready(function () {
         });
     });
 
+    /* Кнопка на моб. устройствах скрыть/показать текст на главной */
     $('#show-main-description').on("click", function () {
         $('#main-description').toggle();
         $(this).text(function(i, text){
           return text === "Читать подробнее" ? "Скрыть" : "Читать подробнее";
       });
     });
+    
     /* Открытие выпадающего списка */
 
     $(".input-select-city").each(function () {
@@ -51,6 +29,102 @@ $(document).ready(function () {
         });
     });
 
+    
+    
+    /* Табы */
+    if(window.matchMedia('(min-width: 768px)').matches){
+        $(function () {
+            $(".tabs-btn-wrap").on("click", ".tabs-btn:not(.active)", function () {
+                $(this)
+                    .addClass("active")
+                    .siblings()
+                    .removeClass("active")
+                    .closest("div.tabs")
+                    .find("div.tabs-content")
+                    .removeClass("active")
+                    .eq($(this).index())
+                    .addClass("active");
+            });
+        });
+    }
+    /* Checkbox */
+    
+    $(function () {
+        $(".form-checkbox-input").change(function () {
+            if ($(this).prop('checked') == true) {
+                
+                $(this).parents().find('.select-birth-time').attr('disabled','disabled');
+                $(this).parent().addClass('checked');
+                $(this).closest('.form-item').find('.birth-time-select').attr('disabled', 'disabled');
+
+            } else if ($(this).prop('checked') == false) {
+                 $(this).parents().find('.select-birth-time').removeAttr('disabled');
+                $(this).parent().removeClass('checked');
+                $(this).closest('.form-item').find('.birth-time-select').removeAttr('disabled');
+                $(this).closest('.form-item').find('.birth-time-select').removeAttr('disabled');
+            }
+        });
+    });
+    
+    /* Popup Window */
+    $('.popup-close').on("click", function () {
+        $('.popup').hide();
+    });
+    $('.popup-open').on("click", function () {
+        $('.popup, .popup-wrap').show();
+    });
+    
+    
+    /* Кнопка пройти тест */
+    
+     $('.start-test-btn').on("click", function () {
+        $('.test-links').toggleClass('shown');
+         $(this).toggleClass('open');
+    });
+    /* Calendar */
+    
+    $('.select-birth-date').focus(function() {
+        $(this).next().show();
+    });
+    $('.select-birth-date').focusout(function() {
+        $(this).next().hide();
+    });
+    $('.close-calendar').on("click", function () {
+        $('.calendar').hide();
+    });
+    
+    if(window.matchMedia('(max-width: 768px)').matches){
+        
+        
+        
+        $(".tabs-btn").on("click", function () {
+            
+            if ($(this).hasClass('active')){
+                $(this).removeClass('active');
+                $(this).parent().removeClass('active');
+            }
+            else{
+                $(".tabs-btn, .tabs-content").removeClass('active');     
+                $(this).toggleClass('active');
+                $(this).parent().toggleClass('active');
+            }
+        });
+   
+         $(".chart-levels-items .btn-show-details").on("click", function () {
+             $(this).siblings('.level-info-text').toggle();
+             $(this).toggle();
+             $(this).siblings('.btn-recommend').toggle();
+        });
+        $('#partner1_tab').on("click", function () {
+            $('.result-partner-wrap').removeClass('active');
+             $('#partner1_result').addClass('active');
+        });
+         $('#partner2_tab').on("click", function () {
+             $('.result-partner-wrap').removeClass('active');
+             $('#partner2_result').addClass('active');
+        });
+    }
+    
     /* График */ 
     $('.chart-level[level-data="l1"]').on("click", function () {
         
@@ -281,101 +355,30 @@ $(document).ready(function () {
     }, 2000);
     });
     
-        
-           
-        
-    
-    /* Табы */
-    if(window.matchMedia('(min-width: 768px)').matches){
-        $(function () {
-            $(".tabs-btn-wrap").on("click", ".tabs-btn:not(.active)", function () {
-                $(this)
-                    .addClass("active")
-                    .siblings()
-                    .removeClass("active")
-                    .closest("div.tabs")
-                    .find("div.tabs-content")
-                    .removeClass("active")
-                    .eq($(this).index())
-                    .addClass("active");
-            });
-        });
-    }
-    /* Checkbox */
-    
-    $(function () {
-        $(".form-checkbox-input").change(function () {
-            if ($(this).prop('checked') == true) {
-                
-                $(this).parents().find('.select-birth-time').attr('disabled','disabled');
-                $(this).parent().addClass('checked');
-                $(this).closest('.form-item').find('.birth-time-select').attr('disabled', 'disabled');
-
-            } else if ($(this).prop('checked') == false) {
-                 $(this).parents().find('.select-birth-time').removeAttr('disabled');
-                $(this).parent().removeClass('checked');
-                $(this).closest('.form-item').find('.birth-time-select').removeAttr('disabled');
-                $(this).closest('.form-item').find('.birth-time-select').removeAttr('disabled');
-            }
-        });
-    });
-    
-    /* Popup Window */
-    $('.popup-close').on("click", function () {
-        $('.popup').hide();
-    });
-    $('.popup-open').on("click", function () {
-        $('.popup, .popup-wrap').show();
-    });
-    
-    
-    /* Кнопка пройти тест */
-    
-     $('.start-test-btn').on("click", function () {
-        $('.test-links').toggleClass('shown');
-         $(this).toggleClass('open');
-    });
-    /* Calendar */
-    
-    $('.select-birth-date').focus(function() {
-        $(this).next().show();
-    });
-    $('.select-birth-date').focusout(function() {
-        $(this).next().hide();
-    });
-    $('.close-calendar').on("click", function () {
-        $('.calendar').hide();
-    });
-    
-    if(window.matchMedia('(max-width: 768px)').matches){
-        
-        
-        
-        $(".tabs-btn").on("click", function () {
-            
-            if ($(this).hasClass('active')){
-                $(this).removeClass('active');
-                $(this).parent().removeClass('active');
-            }
-            else{
-                $(".tabs-btn, .tabs-content").removeClass('active');     
-                $(this).toggleClass('active');
-                $(this).parent().toggleClass('active');
-            }
-        });
-   
-         $(".chart-levels-items .btn-show-details").on("click", function () {
-             $(this).siblings('.level-info-text').toggle();
-             $(this).toggle();
-             $(this).siblings('.btn-recommend').toggle();
-        });
-        $('#partner1_tab').on("click", function () {
-            $('.result-partner-wrap').removeClass('active');
-             $('#partner1_result').addClass('active');
-        });
-         $('#partner2_tab').on("click", function () {
-             $('.result-partner-wrap').removeClass('active');
-             $('#partner2_result').addClass('active');
-        });
-    }
 });
+
+
+
+/* Слайдеры */
+
+let slideIndex = [1,1];
+
+let slideId = ["popup_slider", "page_slider"]
+
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].classList.remove("shown");  
+  }
+  x[slideIndex[no]-1].classList.add("shown");  
+}
